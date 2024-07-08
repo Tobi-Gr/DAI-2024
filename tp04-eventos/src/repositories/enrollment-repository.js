2dbe1fcfb6a76aedd4a79e2c0bbe38a74b53fb30
Android Studio <devandroid@ort>
2024-07-08 10:32:06 -0300

    punto 9a

--- tp04-eventos/src/repositories/enrollment-repository.js
+++ tp04-eventos/src/repositories/enrollment-repository.js
@@ -5,13 +5,27 @@ export default class EnrollmentRepository {
 
     createAsync = async (entity) =>
     {
-        let sql = ``;
+        let sql = `INSERT INTO public.event_enrollments(id_event, id_user, description, registration_date_time, attended, observations, rating)
+                        VALUES($1, $2, $3, $4, $5, $6, $7)`;
         let registration_d_t = new Date();        
         let values = [entity.id_event, entity.id_user, entity.description, registration_d_t, false, null, null];
         return dbh.requestOne(sql, values)? true : false;
     }
 
-    deleteByIdAsync = async (id) => //Id del usuario o del enrollment??
+    //devuelve la cantidad de gente anotada para un evento
+    countEnrolledAsync = async (id_event) =>{
+        let query = 'SELECT COUNT(*) FROM public.event_enrollments WHERE id_event = $1';
+        let values = [id_event];
+        return requestOne(query, values);
+    }
+
+    getByUserId = async (id_user) => {
+        let query = 'SELECT * FROM public.event_enrollments WHERE id_user = $1';
+        let values = [id_user];
+        return requestOne(query, values);
+    }
+
+    deleteByIdAsync = async (id) => //Id del del enrollment
     {
         return dbh.requestOne('DELETE FROM event_enrollments WHERE id = $1', [id])
     }
--- tp04-eventos/src/services/enrollment-service.js
+++ tp04-eventos/src/services/enrollment-service.js
@@ -6,4 +6,14 @@ export default class EnrollmentService {
         const created = await repo.createAsync(entity);
         return created;
     }
+
+    countEnrolledAsync = async (id_event) => {
+        const count = await repo.countEnrolledAsync(id_event);
+        return count;
+    }
+
+    getByIdUser = async (id_user) => {
+        const registration = await repo.countEnrolledAsync(id_user);
+        return registration;
+    }
 }
