b8157fe86cc92ffbd22bff3d42c3938afbee25e4
Android Studio <devandroid@ort>
2024-08-12 09:07:40 -0300

    título

--- tp07-appNavegable/screens/Login.js
+++ tp07-appNavegable/screens/Login.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import Title from './components/Title';  // Corregido el path
+import Title from './components/Title';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Title text="Inicio sesión" />
+      <Title text={"Inicio sesión"} />
     </View>
   );
 }
