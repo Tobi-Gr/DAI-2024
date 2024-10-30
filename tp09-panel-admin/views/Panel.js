801d2e44f60a0819580841597ccb5a2c0ac3b75d
Android Studio <devandroid@ort>
2024-10-30 08:56:47 -0300

    boton secundario, archivo pantalla panel

---
+++ tp09-panel-admin/views/Panel.js
@@ -0,0 +1,9 @@
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { StyleSheet, View, Text } from 'react-native';
+import Boton from '../components/Boton';
+import Title from '../components/Title';
+import React, { useState, useEffect } from 'react';
+
+export default function DetalleEvento() {
+
+}
